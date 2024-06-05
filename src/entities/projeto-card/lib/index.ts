4e2a2ed5fcc90eb8a45c78463b593a0d35a008e7
Bruno Martins <bgmartins@ucs.br>
2024-06-05 11:25:13 -0300

    feat: :lipstick: Adição do logo, de pagina de produções academicas e redesign do card de projeto.

--- src/entities/projeto-card/lib/index.ts
+++ src/entities/projeto-card/lib/index.ts
@@ -1,3 +1,6 @@
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
 export function getStatusLabel(status: Projeto['status']) {
     switch (status) {
         case 'EM_ANDAMENTO':
@@ -10,3 +13,23 @@ export function getStatusLabel(status: Projeto['status']) {
             return '';
     }
 }
+
+export function getIconName(status: Projeto['status']) {
+    switch (status) {
+        case 'EM_ANDAMENTO':
+            return 'construct';
+        case 'CANCELADO':
+            return 'close';
+        case 'CONCLUIDO':
+            return 'checkmark-done-sharp';
+        default:
+            return '';
+    }
+}
+
+
+export const formatDateToTimeAgo = (dateString: Projeto['dataCriacao']) => {
+    const [day, month, year] = dateString.split('/');
+    const date = new Date(`${year}-${month}-${day}`);
+    return formatDistanceToNow(date, { addSuffix: true, locale: ptBR });
+};
--- src/entities/projeto-card/ui/index.tsx
+++ src/entities/projeto-card/ui/index.tsx
@@ -1,12 +1,15 @@
+import { Button } from '@/shared/ui/button';
 import {
     Card,
     CardContent,
     CardDescription,
+    CardFooter,
     CardHeader,
     CardTitle,
 } from '@/shared/ui/card';
 import { IonIcon } from '@/shared/ui/ion-icon';
 import Link from 'next/link';
+import { formatDateToTimeAgo, getIconName } from '../lib';
 import './paper-flip.scss';
 
 type ProjetoCardProps = {
@@ -17,59 +20,67 @@ export function ProjetoCard({ projeto }: ProjetoCardProps) {
     return (
         <Link href={`/projetos/${projeto.id}`}>
             <Card
-                className="cursor-pointer overflow-hidden rounded-sm border-0 border-l-4 shadow-md transition-transform duration-200 hover:scale-[1.02] hover:shadow-lg"
+                className="overflow-hidden rounded-sm border-0 border-l-4 shadow-md transition-transform duration-200 hover:shadow-lg"
                 style={{
                     borderColor: projeto.area.color,
                 }}
             >
                 <CardHeader className="relative">
-                    <div className="flex items-center justify-start">
+                    <div className="flex items-center justify-between">
                         <CardTitle className="text-xl">
                             {projeto.titulo}
                         </CardTitle>
+                        <Button variant={'link'}>
+                            <IonIcon name="ellipsis-vertical" size="small" />
+                        </Button>
                     </div>
                     <CardDescription className="objetivoTruncado">
                         {projeto.objetivo}
                     </CardDescription>
                     {/* paper flip effect done very wrong */}
-                    <div className="absolute right-0 top-0 flex h-14 w-14 items-start justify-end pr-2 pt-2">
+                    <div className="absolute right-0 top-0 flex items-start justify-end pr-2 pt-2">
                         <div
                             className="paper-flip border-l-[3.5rem] border-t-[3.5rem]"
                             data-status={projeto.status}
                         ></div>
                         <IonIcon
-                            name="close-outline"
+                            name={getIconName(projeto.status)}
                             size="small"
                             className="text-white"
                         />
                     </div>
                 </CardHeader>
                 <CardContent>
-                    <div className="flex items-center gap-2">
-                        <div className="flex h-[1.125rem] w-[1.125rem] items-center justify-center">
-                            <span
-                                className="h-3 w-3 rounded-full opacity-85"
-                                style={{
-                                    backgroundColor: projeto.area.color,
-                                }}
+                    <div className="flex items-center gap-3 text-muted">
+                        <div className="flex items-center gap-2 truncate">
+                            <IonIcon
+                                name="person-outline"
+                                size="small"
+                                className="shrink-0"
                             />
+                            <span className="truncate text-sm">
+                                {projeto.coordenador.nome}{' '}
+                                {projeto.coordenador.sobrenome}
+                            </span>
                         </div>
-                        <span className="text-sm text-muted">
+                        <div className="h-1 w-1 shrink-0 rounded-full bg-muted"></div>
+                        <span className="text-nowrap text-sm">
+                            {formatDateToTimeAgo(projeto.dataCriacao)}
+                        </span>
+                    </div>
+                </CardContent>
+                <CardFooter className="relative">
+                    <div
+                        className="absolute bottom-2 left-0 flex items-center gap-2 rounded-r-md px-2 py-[.125rem] shadow-md"
+                        style={{
+                            backgroundColor: projeto.area.color,
+                        }}
+                    >
+                        <span className="text-xs text-white">
                             {projeto.area.nome}
                         </span>
                     </div>
-                    <div className="flex items-center gap-2 text-muted">
-                        <IonIcon name="person-outline" size="small" />
-                        <span className="text-sm">
-                            {projeto.coordenador.nome}{' '}
-                            {projeto.coordenador.sobrenome}
-                        </span>
-                    </div>
-                    <div className="flex items-center gap-2 text-muted">
-                        <IonIcon name="calendar-outline" size="small" />
-                        <span className="text-sm">{projeto.dataCriacao}</span>
-                    </div>
-                </CardContent>
+                </CardFooter>
             </Card>
         </Link>
     );
